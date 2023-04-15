5caae21e210c5918791ef2c2d179ce073a48c8aa
seungho <slkumquat@gmail.com>
2023-04-15 21:00:50 +0900

    update 02-grid-mapping

--- msr1-02-grid-mapping/ex3.py
+++ msr1-02-grid-mapping/ex3.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import bresenham as bh
 
 def plot_gridmap(gridmap):
-    plt.figure()
+    plt.figure(figsize=(10,10))
     plt.imshow(gridmap, cmap='Greys',vmin=0, vmax=1)
     
 def init_gridmap(size, res):
@@ -15,9 +15,17 @@ def init_gridmap(size, res):
 
 def world2map(pose, gridmap, map_res):
     origin = np.array(gridmap.shape)/2
-    new_pose = np.zeros(1,2)
-    new_pose[0] = np.round(pose[0]/map_res) + origin[0];
-    new_pose[1] = np.round(pose[1]/map_res) + origin[1];
+    if pose.size == 3:
+        new_pose = np.zeros(2)
+        
+        new_pose[0] = np.round(pose[0]/map_res) + origin[0]
+        new_pose[1] = np.round(pose[1]/map_res) + origin[1]
+        
+    else:
+        new_pose = np.zeros((2, pose.shape[1]))
+        
+        new_pose[0, :] = np.round(pose[0, :]/map_res) + origin[0]
+        new_pose[1, :] = np.round(pose[1, :]/map_res) + origin[1]
     return new_pose.astype(int)
 
 def v2t(pose):
@@ -60,14 +68,45 @@ def bresenham(x0, y0, x1, y1):
     l = np.array(list(bh.bresenham(x0, y0, x1, y1)))
     return l
     
-# def prob2logodds(p):
-    # add code here
-    
-# def logodds2prob(l):
-    # add code here    
-    
-# def inv_sensor_model(cell, endpoint, prob_occ, prob_free):
-    # add code here
+def logodds2prob(log_odds):
+    prob = 1 -  (1 / (1 + np.exp(log_odds)))
+    return prob
 
-# def grid_mapping_with_known_poses(ranges_raw, poses_raw, occ_gridmap, map_res, prob_occ, prob_free, prior):
-    # add code here
+def prob2logodds(prob):
+    if prob == 1:
+        prob = 0.99
+    log_odds = np.log(prob / (1 - prob))
+    return log_odds
+    
+def inv_sensor_model(cell, endpoint, prob_occ, prob_free):
+    check_cell = bresenham(cell[0], cell[1], endpoint[0], endpoint[1])
+    if check_cell.shape[0] == 0:
+        free_cell = np.array([])
+        occ_cell = np.array([])
+    elif check_cell.shape[0] == 1:
+        free_cell = np.array([])
+        occ_cell = np.array([check_cell[0,0], check_cell[0, 1]])
+    elif check_cell.shape[0] > 1:
+        free_cell = np.array(check_cell[0:-1,:])
+        occ_cell = np.array(check_cell[-1,:])
+    return free_cell, occ_cell
+
+def grid_mapping_with_known_poses(ranges_raw, poses_raw, occ_gridmap, map_res, prob_occ, prob_free, prior):
+    m_current_point = poses2cells(poses_raw, occ_gridmap, map_res)
+    m_points = ranges2cells(ranges_raw, poses_raw, occ_gridmap, map_res)
+
+    for i in range(m_points.shape[1]):
+        x_ = m_points[0, i]
+        y_ = m_points[1, i]
+        
+        free_cell, occ_cell = inv_sensor_model(m_current_point, [x_, y_], prob_occ, prob_free)
+        
+        # free_cell
+        for j in range(free_cell.shape[0]):
+            log_odds_ = prob2logodds(occ_gridmap[free_cell[j,0], free_cell[j,1]]) + prob2logodds(prob_free) - prob2logodds(prior)
+            occ_gridmap[free_cell[j,0], free_cell[j,1]] = logodds2prob(log_odds_)
+        # occ_cell
+        log_odds_ = prob2logodds(occ_gridmap[occ_cell[0], occ_cell[1]]) + prob2logodds(prob_occ) - prob2logodds(prior)
+        occ_gridmap[occ_cell[0], occ_cell[1]] = logodds2prob(log_odds_)
+    
+    return occ_gridmap
